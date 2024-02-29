0c466bb806a3c0458f295bdcc6e44a87c51ddb59
rumdien113 <rumdien113@gmail.com>
2024-02-29 20:18:23 +0700

    fix get info of user

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -9,7 +9,11 @@ class AccountSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = Account(
             username = validated_data['username'],
-            email = validated_data['email']
+            email = validated_data['email'],
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name'],
+            date_of_birth = validated_data['date_of_birth'],
+            gender = validated_data['gender']
         )
         user.set_password(validated_data['password'])
         user.save()
